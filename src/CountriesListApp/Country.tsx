21731ce6076852e86726f709f11f4257661633d2
binhnguyen2501 <binhq6@gmail.com>
2022-02-21 15:29:05 +0700

    update map each country

--- src/CountriesListApp/Country.tsx
+++ src/CountriesListApp/Country.tsx
@@ -56,87 +56,70 @@ const Country: React.FC = () => {
   }, [capital]);
 
   return (
-    <div className="bg-slate-100">
-      <div className="pt-12 xl:max-w-7xl xl:mx-auto px-5 xl:px-0 h-screen">
-        <Link
-          to={"/CountriesListApp"}
-          className="bg-[#EF4638] pt-2 pb-3 pl-4 pr-6 rounded shadow text-white font-bold tracking-wide"
-        >
-          &larr; Back
-        </Link>
-        <div className="mt-12">
-          {isLoading && (
-            <div className="text-center text-xl lg:text-2xl font-bold">
-              Loading...
-            </div>
-          )}
-          {!isLoading &&
-            country.map((item: CountryTypes, index: number) => (
-              <div key={index} className="flex flex-col lg:flex-row">
-                <div className="w-full lg:w-2/5">
-                  <div>
-                    <img src={item.flag} alt={item.countryName} />
-                    <div className="text-4xl lg:text-6xl font-bold text-gray-900 mt-4">
-                      {item.countryName},{" "}
-                      <span className="font-light text-2xl lg:text-4xl">
-                        {item.capital}
-                      </span>
-                    </div>
-                  </div>
-                  <div className="text-xl font-semibold mt-2">
-                    {item.countryOfficialName}
-                  </div>
-                  <div className="text-lg flex items-center gap-3 mt-3">
-                    <span className="text-[#EF4638] font-semibold">
-                      Coat Of Arms:
-                    </span>
-                    {item.coatOfArms ? (
-                      <img
-                        src={item.coatOfArms}
-                        alt="Coat Of Arms"
-                        className="w-12"
-                      />
-                    ) : (
-                      "none"
-                    )}
-                  </div>
-                  <div className="text-lg">
-                    <span className="text-[#EF4638] font-semibold">
-                      Region:
-                    </span>{" "}
-                    {item.region}
-                  </div>
-                  <div className="text-lg">
-                    <span className="text-[#EF4638] font-semibold">
-                      Subregion:
-                    </span>{" "}
-                    {item.subregion}
-                  </div>
-                  <div className="text-lg">
-                    <span className="text-[#EF4638] font-semibold">
-                      Languages:
-                    </span>{" "}
-                    {item.languages}
-                  </div>
-                  <div className="text-lg">
-                    <span className="text-[#EF4638] font-semibold">
-                      Population:
-                    </span>{" "}
-                    {item.population.toLocaleString()}
-                  </div>
-                  <div className="text-lg">
-                    <span className="text-[#EF4638] font-semibold">
-                      Top Level Domain:
-                    </span>{" "}
-                    {item.topLevelDomain}
-                  </div>
+    <div className="xl:py-10 xl:px-14 p-6 min-h-screen bg-slate-100">
+      <Link
+        to={"/CountriesListApp"}
+        className="bg-[#EF4638] pt-2 pb-3 pl-4 pr-6 rounded shadow text-white font-bold tracking-wide"
+      >
+        &larr; Back
+      </Link>
+      <div className="mt-10">
+        {isLoading && (
+          <div className="text-center text-xl lg:text-2xl font-bold">
+            Loading...
+          </div>
+        )}
+        {!isLoading &&
+          country.map((item: CountryTypes, index: number) => (
+            <div key={index} className="flex flex-col gap-2 xl:flex-row">
+              <div className="w-full xl:w-1/2">
+                <img src={item.flag} alt={item.countryName} />
+                <div className="text-4xl lg:text-6xl font-extrabold text-gray-900 mt-4">
+                  {item.countryName},{" "}
+                  <span className="font-light text-2xl lg:text-4xl">
+                    {item.capital}
+                  </span>
                 </div>
-                <div className="flex-1">
-                  <GoogleMaps location={item.map} />
+                <div className="text-xl font-bold mt-2">
+                  {item.countryOfficialName}
+                </div>
+                <div className="text-md flex items-center gap-3 mt-3 font-light">
+                  <span className="font-semibold">Coat Of Arms:</span>
+                  {item.coatOfArms ? (
+                    <img
+                      src={item.coatOfArms}
+                      alt="Coat Of Arms"
+                      className="w-12"
+                    />
+                  ) : (
+                    "none"
+                  )}
+                </div>
+                <div className="text-md font-light">
+                  <span className="font-semibold">Region:</span> {item.region}
+                </div>
+                <div className="text-md font-light">
+                  <span className="font-semibold">Subregion:</span>{" "}
+                  {item.subregion}
+                </div>
+                <div className="text-md font-light">
+                  <span className="font-semibold">Languages:</span>{" "}
+                  {item.languages}
+                </div>
+                <div className="text-md font-light">
+                  <span className="font-semibold">Population:</span>{" "}
+                  {item.population.toLocaleString()}
+                </div>
+                <div className="text-md font-light">
+                  <span className="font-semibold">Top Level Domain:</span>{" "}
+                  {item.topLevelDomain}
                 </div>
               </div>
-            ))}
-        </div>
+              <div className="w-full h-[800px] xl:h-auto">
+                <GoogleMaps location={item.map} />
+              </div>
+            </div>
+          ))}
       </div>
     </div>
   );
--- src/components/GoogleMaps.tsx
+++ src/components/GoogleMaps.tsx
@@ -1,5 +1,4 @@
-import { useRef } from "react";
-import { GoogleMap, useJsApiLoader } from "@react-google-maps/api";
+import GoogleMapReact from "google-map-react";
 
 interface Props {
   location: {
@@ -8,40 +7,17 @@ interface Props {
   };
 }
 
-const options = {
-  disableDefaultUI: true,
-  zoomControl: true,
-};
-
 const GoogleMaps = ({ location }: Props) => {
-  const { isLoaded } = useJsApiLoader({
-    id: "google-map-script",
-    googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAP_KEY!,
-  });
-
-  //save map in ref if we want to access the map
-  const mapRef = useRef<any | null>(null);
-  const onLoad = (map: any): void => {
-    mapRef.current = map;
-  };
-  const onUnMount = (): void => {
-    mapRef.current = null;
-  };
-
-  if (!isLoaded) return <div>Map Loading...</div>;
   return (
-    <>
-      <GoogleMap
-        options={options as google.maps.MapOptions}
-        center={{
-          lat: location.lat,
-          lng: location.long,
-        }}
-        zoom={12}
-        onLoad={onLoad}
-        onUnmount={onUnMount}
-      />
-    </>
+    <GoogleMapReact
+      bootstrapURLKeys={{ key: process.env.REACT_APP_GOOGLE_MAP_KEY! }}
+      defaultCenter={{
+        lat: location.lat,
+        lng: location.long,
+      }}
+      defaultZoom={6}
+      yesIWantToUseGoogleMapApiInternals
+    ></GoogleMapReact>
   );
 };
 
